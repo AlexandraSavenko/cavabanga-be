29a8b611534cff56a3f86d1c77e22dad2abff263
SerbynS <serbin.sasha1@gmail.com>
2025-08-19 23:32:14 +0300

    base-structure

---
+++ src/routers/index.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+const router = Router();
+
+// Приклад:
+// router.use('/contacts', contactsRouter);
+// router.use('/auth', authRouter);
+
+export default router;
---
+++ src/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+
+import pino from 'pino-http';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import router from './routers/index.js';
+
+const PORT = process.env.PORT || 3000;
+
+export function setupServer() {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+  app.use(cookieParser());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use(router);
+  // app.use(notFoundHandler);
+
+  // app.use(errorHandler);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
