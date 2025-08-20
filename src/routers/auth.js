303ba0883f232c1820c4805246a565d96864b7ac
hanna-uler <hannauler@gmail.com>
2025-08-20 16:24:54 -0400

    add validation on reg and login routes

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-// import { registerUserSchema, loginUserSchema } from "../validation/auth.js";
+import { registerUserSchema, loginUserSchema } from "../validation/auth.js";
 import { registerUserController, loginUserController } from "../controllers/auth.js";
+import { validateBody } from "../middlewares/validateBody.js";
 
 const router = Router();
 
-router.post('/register', registerUserController);
-router.post('/login', loginUserController);
+router.post('/register', validateBody(registerUserSchema), registerUserController);
+router.post('/login', validateBody(loginUserSchema), loginUserController);
 
 export default router;
