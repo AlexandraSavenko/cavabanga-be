04b867ad4652d7388196631b20c5d59f195400fb
hanna-uler <hannauler@gmail.com>
2025-08-21 10:06:38 -0400

    add logout

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { registerUserSchema, loginUserSchema } from "../validation/auth.js";
-import { registerUserController, loginUserController } from "../controllers/auth.js";
+import { registerUserController, loginUserController, logoutUserController } from "../controllers/auth.js";
 import { validateBody } from "../middlewares/validateBody.js";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 
@@ -8,5 +8,5 @@ const router = Router();
 
 router.post('/register', validateBody(registerUserSchema), ctrlWrapper(registerUserController));
 router.post('/login', validateBody(loginUserSchema), ctrlWrapper(loginUserController));
-
+router.post('/logout', ctrlWrapper(logoutUserController));
 export default router;
