113313d7cc67a6fe1424c5c0b603fc23781b8cf6
Tania Dumych <tanja11.malvchko@gmail.com>
2025-08-21 18:09:33 +0200

    Revert "Revert "recipes (#28)" (#29)"
    This reverts commit ed8721e0da340937f78b48bab67cc61437996061.

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,16 +1,11 @@
 import mongoose from 'mongoose';
 import { getEnvVar } from '../utils/getEnvVar.js';
 
-const MONGODB_USER = getEnvVar('MONGODB_USER');
-const MONGODB_PASSWORD = getEnvVar('MONGODB_PASSWORD');
 const MONGODB_URL = getEnvVar('MONGODB_URL');
-const MONGODB_DB = getEnvVar('MONGODB_DB');
 
 export async function initMongoConnection() {
-  const mongoUri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
-
   try {
-    await mongoose.connect(mongoUri);
+    await mongoose.connect(MONGODB_URL);
 
     console.log('Mongo connection successfully established!');
   } catch (error) {
