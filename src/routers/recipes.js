c27929bcfc77bb798075edc034a519d7350b9651
valentina2209 <valya253@ukr.net>
2025-08-25 10:19:59 +0300

    Private recipes  (#44)
    * Add private endpoints for user's own recipes
    
    * feat: created a private endpoint to retrieve your own recipes
    
    * add model recipes, own
    
    ---------
    
    Co-authored-by: SerbynS <serbin.sasha1@gmail.com>
    Co-authored-by: Oleksandr <103267560+SerbynS@users.noreply.github.com>

--- src/routers/recipes.js
+++ src/routers/recipes.js
@@ -1,9 +1,11 @@
-import { Router } from 'express';
-import { getRecipeByIdController } from '../controllers/recipes.js';
+import { Router } from "express";
+import { getRecipesController } from '../controllers/recipesController.js';
+import { authenticate } from '../middlewares/authenticate.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
+router.get('/own', authenticate, ctrlWrapper(getRecipesController));
 router.get('/:id', ctrlWrapper(getRecipeByIdController));
 
 export default router;
