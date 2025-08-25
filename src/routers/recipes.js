44a27eeb5fcbace5e4f6550fcf41b06e70ddae02
SerbynS <serbin.sasha1@gmail.com>
2025-08-25 12:08:23 +0300

    fixed problem import

--- src/routers/recipes.js
+++ src/routers/recipes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { getRecipesController } from '../controllers/recipesController.js';
+import { getRecipeByIdController } from '../controllers/recipes.js';
 import { authenticate } from '../middlewares/authenticate.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
 router.get('/own', authenticate, ctrlWrapper(getRecipesController));
-router.get('/:id', ctrlWrapper(getRecipesController));
+router.get('/:id', ctrlWrapper(getRecipeByIdController));
 
 export default router;
