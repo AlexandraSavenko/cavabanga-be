473f2bcbd7cb515ac3d82d67eb4f1503dea199a5
SerbynS <serbin.sasha1@gmail.com>
2025-08-25 12:03:34 +0300

    add router getUserInfo

--- src/routers/recipes.js
+++ src/routers/recipes.js
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router } from 'express';
 import { getRecipesController } from '../controllers/recipesController.js';
 import { authenticate } from '../middlewares/authenticate.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
@@ -6,6 +6,6 @@ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 const router = Router();
 
 router.get('/own', authenticate, ctrlWrapper(getRecipesController));
-router.get('/:id', ctrlWrapper(getRecipeByIdController));
+router.get('/:id', ctrlWrapper(getRecipesController));
 
 export default router;
