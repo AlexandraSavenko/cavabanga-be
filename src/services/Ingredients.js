e05f93c7b01a5ec204767ad5616942706574efb6
SerbynS <serbin.sasha1@gmail.com>
2025-08-27 10:57:32 +0300

    fixed

--- src/services/Ingredients.js
+++ src/services/Ingredients.js
@@ -1,5 +1,4 @@
 import { IngredientsCollection } from '../db/models/ingredient.js';
-
 export const getIngredients = async (req, res) => {
   return IngredientsCollection.find().sort({ name: 1 });
 };
